77810b47e721e4d789a68c2b3375499c45c0ca0b
Ashutosh <ashutoshtips@gmail.com>
2024-06-12 00:01:09 +0530

    Update Install-DVWA.sh

--- Install-DVWA.sh
+++ Install-DVWA.sh
@@ -273,8 +273,3 @@ credentials_after_setup_message=$( "\e[92mCredentials:\e[0m")
 echo -e "$credentials_after_setup_message"
 echo -e "Username: \033[93madmin\033[0m"
 echo -e "Password: \033[93mpassword\033[0m"
-
-#  End of installer
-echo
-final_message=$( "With ♡ by its-ashu-otf")
-echo -e "$final_message"
